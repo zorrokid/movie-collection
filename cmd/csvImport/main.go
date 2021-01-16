40cdc67ba2941398914e79dcd722fc1dfa82ec43
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-01-16 20:07:09 +0200

    fetch data

--- cmd/csvImport/main.go
+++ cmd/csvImport/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/zorrokid/movieCollection/pkg/database"
+	"github.com/zorrokid/movieCollection/pkg/models"
 )
 
 func main() {
-	_, err := database.InitDB()
+	db, err := database.InitDB()
 	if err != nil {
 		panic("failed connecting")
 	}
 
+	condition := models.Condition{}
+
+	db.First(&condition)
+	fmt.Printf("Got %s", condition.Name)
+
+	var conditions []models.Condition
+
+	db.Find(&conditions)
+	for _, c := range conditions {
+		fmt.Printf("Got %s", c.Name)
+	}
 }
